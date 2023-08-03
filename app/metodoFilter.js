da10f732a46ad630b1980578bda26809b342076a
Lucas <lucasgusso28@gmail.com>
2023-08-02 22:22:15 -0300

    Aula 4 - vídeo 2 e 3

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,11 +1,3 @@
-// const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
-// const btnFiltrarLivrosDeBack = document.getElementById('btnFiltrarLivrosBack');
-// const btnFiltrarLivrosDeDados = document.getElementById('btnFiltrarLivrosDados');
-
-// btnFiltrarLivrosDeFront.addEventListener('click',filtrarLivrosFront);
-// btnFiltrarLivrosDeBack.addEventListener('click',filtrarLivrosBack);
-// btnFiltrarLivrosDeDados.addEventListener('click',filtrarLivrosDados);
-
 const botoes = document.querySelectorAll('.btn');
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 
@@ -16,36 +8,3 @@ function filtrarLivros() {
     exibirLivrosNaTela(livrosFiltrados);
 }
 
-// function exibeLivrosFiltrados(listaDeLivros) {
-//     elementoParaInserirLivros.innerHTML = ''
-
-//     listaDeLivros.forEach(livro => {
-        
-//         elementoParaInserirLivros.innerHTML += `
-//         <div class="livro">
-//         <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-//         <h2 class="livro__titulo">
-//             ${livro.titulo}
-//         </h2>
-//         <p class="livro__descricao">${livro.autor}</p>
-//         <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
-//         <div class="tags">
-//             <span class="tag">${livro.categoria}</span>
-//         </div>
-//         </div>
-//             `
-
-//     });
-// }
-
-// function filtrarLivrosFront() {
-//     filtrarLivros('front-end');
-// }
-
-// function filtrarLivrosBack() {
-//     filtrarLivros('back-end');
-// }
-
-// function filtrarLivrosDados() {
-//     filtrarLivros('dados');
-// }
